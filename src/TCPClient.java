8d8252b32864dd9b38770234546235092241df21
sahilappay <sahil.appayev@mail.ru>
2020-01-30 16:45:35 +0400

    new commit

--- src/TCPClient.java
+++ src/TCPClient.java
@@ -1,47 +1,63 @@
+
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class TCPClient {
+
     public static void main(String[] args) throws Exception {
-        clientFileAsByte();
+        clientFileAsByteNew();
     }
 
-    public static void clientText() throws Exception{
-        try (Socket clientSocket = new Socket("localhost", 6789)){//Autoclosable
-            DataOutputStream outToStream = new DataOutputStream(clientSocket.getOutputStream());// melumati gonderir
-            Scanner sc =new Scanner(System.in);
+    public static void clientText() throws Exception {
+        try (Socket clientSocket = new Socket("localhost", 6789)) {//Autoclosable
+            OutputStream os = clientSocket.getOutputStream();
+            DataOutputStream outToStream = new DataOutputStream(os);// melumati gonderir
+            Scanner sc = new Scanner(System.in);
             System.out.println("Ener value:");
             String text = sc.nextLine();
-            outToStream.writeBytes(text);
+            outToStream.write(text.getBytes());
             System.out.println(text);
         }
     }
 
-    public static void clientFileAsByte() throws Exception{
-        try (Socket clientSocket = new Socket("localhost", 6789)){//Autoclosable
+    public static void clientFileAsByte() throws Exception {
+        try (Socket clientSocket = new Socket("localhost", 6789)) {//Autoclosable
             DataOutputStream outToStream = new DataOutputStream(clientSocket.getOutputStream());// melumati gonderir
-            Scanner sc =new Scanner(System.in);
+            Scanner sc = new Scanner(System.in);
             System.out.println("Ener value:");
             String text = sc.nextLine();
-            byte [] bytes = readUsingByte(text);
+            byte[] bytes = readBytes(text);
             outToStream.write(bytes);
             System.out.println(text);
         }
     }
 
-    public static byte[] readUsingByte(String filePath){
-        byte[] bytesArr = null;
-        try(FileInputStream fileInputStream = new FileInputStream(filePath)){
-            File file = new File(filePath);
-            bytesArr = new byte[(int) file.length()];
-            fileInputStream.read(bytesArr);
-        }catch (Exception e){
-            e.printStackTrace();
+    public static byte[] readBytes(String fileName) throws Exception {/// yarimchiq
+        File file = new File(fileName);
+        try (FileInputStream fis = new FileInputStream(file)) {
+            byte[] bytes = new byte[(int) file.length()];
+            fis.read(bytes);
+            return bytes;
         }
-        return bytesArr;
+    }
+
+    ////////////////NEW METHODS\\\\\\\\\\\\\\\\
+    public static void clientFileAsByteNew() throws Exception {
+        try (Socket clientSocket = new Socket("localhost", 6789)) {//Autoclosable
+            OutputStream os = clientSocket.getOutputStream();
+            DataOutputStream outToStream = new DataOutputStream(os);// melumati gonderir
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Ener value:");
+            String file = sc.nextLine();
+            byte[] bytes = readBytes(file);
+            outToStream.writeInt(bytes.length);
+            outToStream.write(bytes);
+            System.out.println("Done!");
+        }
     }
 }
